312857d08c75ff53b118b00199eeb7bc1b4f5531
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-03-14 15:55:24 -0400

    made adjustments to class for multiple choice

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,17 @@ class multipleChoice{
         this.question = question;
         this.rightAnswer = rightAnswer;
         this.wrongAnswers = wrongAnswers;
+        this.answers = rightAnswer.concat(wrongAnswers);
+    }
+
+    generateQuestion(){
+        
     }
 }
 
 q1 = "What's 1 + 1";
-q1Correct = 2;
+q1Correct = [2];
 q1Incorrect = [1, 4, 5];
 
 const question1 = new multipleChoice(q1, q1Correct, q1Incorrect);
+
