fcd6e3f6e8ee7cb76831ebd48f3b6db2e18ee80e
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-03-14 16:24:35 -0400

    created "generateQuestion" function

--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,13 @@ class multipleChoice{
         this.answers = rightAnswer.concat(wrongAnswers);
     }
 
-    generateQuestion(multipleChoice){
-        
+    generateQuestion(){
+        document.getElementById("Question").innerHTML = this.question;
+        shuffle(this.answers);
+        document.getElementById("option1").innerHTML = this.answers[0];
+        document.getElementById("option2").innerHTML = this.answers[1];
+        document.getElementById("option3").innerHTML = this.answers[2];
+        document.getElementById("option4").innerHTML = this.answers[3];
     }
 }
 
@@ -16,4 +21,18 @@ q1Correct = [2];
 q1Incorrect = [1, 4, 5];
 
 const question1 = new multipleChoice(q1, q1Correct, q1Incorrect);
+question1.generateQuestion();
+
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    while (currentIndex != 0) {
+  
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  }
 
