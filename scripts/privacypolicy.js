1ac1a452324aab1aa0cca1816327b94bb09080fa
Andrew Abdulaziz <andrewalloniealt@gmail.com>
2025-03-25 21:09:22 -0400

    added adjust font to privacy policy (second beast slain)

--- scripts/privacypolicy.js
+++ scripts/privacypolicy.js
@@ -12,3 +12,37 @@ document.getElementById("darkMode").addEventListener("click", function(){
 document.getElementById("guide").addEventListener("click", function(){
     window.open("../guide.html");
 })
+
+plusminus = 0;
+
+document.getElementById("minun").addEventListener("click", function(){
+    if(plusminus > -0.5){
+        plusminus-=0.5;
+        ps = document.getElementsByTagName("p");
+        h1s = document.getElementsByTagName("h1");
+        newPSize = `${1.5 + plusminus}`;
+        newh1size = `${3.5 + plusminus}`;
+        for(i = 0; i < ps.length; i++){
+            ps[i].style.fontSize = `${newPSize}em`;
+        }
+        for(i = 0; i < h1s.length; i++){
+            h1s[i].style.fontSize = `${newh1size}em`;
+        }
+    }
+});
+
+document.getElementById("plusle").addEventListener("click", function(){
+    if(plusminus < 0.5){
+        plusminus+=0.5;
+        ps = document.getElementsByTagName("p");
+        h1s = document.getElementsByTagName("h1");
+        newPSize = `${1.5 + plusminus}`;
+        newh1size = `${3.5 + plusminus}`;
+        for(i = 0; i < ps.length; i++){
+            ps[i].style.fontSize = `${newPSize}em`;
+        }
+        for(i = 0; i < h1s.length; i++){
+            h1s[i].style.fontSize = `${newh1size}em`;
+        }
+    }
+});
